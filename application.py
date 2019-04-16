424a95c44794c304e854c89756a90fa961bfa5d4
moscwa <yatsubayashi09@outlook.jp>
2019-04-16 21:44:01 +0900

    update

--- application.py
+++ application.py
@@ -1,10 +1,34 @@
-from flask import Flask
+import os
+from flask import *
+from janome.tokenizer import Tokenizer
+t = Tokenizer()
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World! ver2"
 
-@app.route("/html")
-def SampleHTML():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def Main():
+    print(request.method)
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        result="不明"
+        sentence=request.form['sentence']
+        cnt=0
+        noun_cnt=0
+        for token in t.tokenize(sentence):
+            cnt+=1
+            if token.part_of_speech.split(',')[0]=="名詞":
+                noun_cnt+=1
+        
+        print(noun_cnt/cnt)
+        if noun_cnt/cnt<0.35:#結構適当
+            result="小説/物語"
+        else:
+            result="ニュース記事"
+        return render_template("result.html",sentence=sentence,result=result)
+
+
+if __name__ == "__main__":
+    #    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
